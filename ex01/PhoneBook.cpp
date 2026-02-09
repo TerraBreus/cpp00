53ceb35dbe023c01a3534d265705c1123065e2ff
TerraBreus <studentbreus@gmail.com>
2026-02-09 14:57:11 +0100

    feat: Constructor for PhoneBook

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,19 +6,27 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 10:21:24 by zivanov           #+#    #+#             */
-/*   Updated: 2026/02/09 12:31:32 by zivanov          ###   ########.fr       */
+/*   Updated: 2026/02/09 14:38:23 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 #include <iostream>
 
+PhoneBook::PhoneBook() : index(-1)
+{
+	std::cout << "W E L C O M E  T O  T H E  8 0 ' S\n";
+}
+
 void	PhoneBook::add()
 {
-	std::cout << "add function implemented here\n";
+	index = (index + 1) % MAX_CONTACTS;
+	std::cout << index << "\n";
+	contacts[index].setContactDetails();
 }
 
 void	PhoneBook::search()
 {
 	std::cout << "SEARCH function WIP\n";
 }
+
