b9849ddea2e44e91e8aa8fb54a63576e8536249a
TerraBreus <studentbreus@gmail.com>
2026-02-09 14:57:29 +0100

    feat: adding contacts and index variable to PhoneBook class

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,21 +6,23 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 10:46:51 by zivanov           #+#    #+#             */
-/*   Updated: 2026/02/09 12:31:32 by zivanov          ###   ########.fr       */
+/*   Updated: 2026/02/09 14:36:39 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 # define MAX_CONTACTS 8
+# include "Contact.hpp"
 # include <iostream>
 
 class PhoneBook
 {
 	private :
-		//Contact	contacts[MAX_CONTACTS];
-		//int		index;
+		Contact	contacts[MAX_CONTACTS];
+		int		index;
 	public :
+		PhoneBook();
 		void	add();
 		void	search();
 };
