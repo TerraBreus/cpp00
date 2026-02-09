2549a29eec8099ffc25b0990db1b344399c80046
TerraBreus <studentbreus@gmail.com>
2026-02-09 14:56:25 +0100

    fix: prototype of setContactDetails for Contact class

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -6,14 +6,14 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 11:07:51 by zivanov           #+#    #+#             */
-/*   Updated: 2026/02/09 11:17:26 by zivanov          ###   ########.fr       */
+/*   Updated: 2026/02/09 14:28:22 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
 #include <iostream>
 
-void	setContactDetails()
+void	Contact::setContactDetails()
 {
 	std::cout << "Just checking\n";
 }
