43378b5bd155209d25a23ce19b6c1e72980ae9eb
TerraBreus <studentbreus@gmail.com>
2026-02-09 14:03:11 +0100

    feat: draft on Contact class

---
+++ ex01/Contact.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/09 10:48:54 by zivanov           #+#    #+#             */
+/*   Updated: 2026/02/09 11:23:16 by zivanov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+# include <string>
+
+class Contact
+{
+	private :
+		std::string firstName;
+		std::string	lastName;
+		std::string	nickName;
+		std::string	phoneNumber;
+		std::string	darkestSecret;
+	public :
+};
+#endif
