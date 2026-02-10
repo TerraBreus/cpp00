1aabfb34718760f747c96d19e9c2207e58075d8f
TerraMint <studentbreus@gmail.com>
2026-02-10 12:07:00 +0100

    feat: added draft for searchCommand

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -13,7 +13,7 @@
 #include "PhoneBook.hpp"
 #include <iostream>
 
-PhoneBook::PhoneBook() : index(-1)
+PhoneBook::PhoneBook() : index(-1), numContacts(0)
 {
 	std::cout << "W E L C O M E  T O  T H E  8 0 ' S\n";
 }
@@ -23,10 +23,32 @@ void	PhoneBook::add()
 	index = (index + 1) % MAX_CONTACTS;
 	std::cout << "Adding Contact with index number:" << index << "\n";
 	contacts[index].setContactDetails();
+	if (numContacts < MAX_CONTACTS)
+		numContacts++;
+
+}
+
+void	PhoneBook::printBriefContactDetails()
+{
+	for (int i = 0; i < numContacts; i++)
+	{
+		std::cout << "Printing index " << i << std::endl;
+	}
 }
 
 void	PhoneBook::search()
 {
-	std::cout << "SEARCH function WIP\n";
+	printBriefContactDetails();
+	/*
+	 * Print Initial Contact information
+	 * index - firstName - lastName - nickName
+	 *
+	 * ask for index.
+	 * check if index is in bounds.
+	 * if not, print initial contact information again
+	 * and ask for input once more
+	 *
+	 * if valid index displayAllContactDetails
+	 */
 }
 
