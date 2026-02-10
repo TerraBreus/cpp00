daf99171cb20a0441d466703abfe10ae41057918
TerraMint <studentbreus@gmail.com>
2026-02-10 10:56:12 +0100

    feat: adding functions to set contact details

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -12,8 +12,44 @@
 
 #include "Contact.hpp"
 #include <iostream>
+#include <string>
+
+int		invalidInput(std::string str)
+{
+	for (size_t i = 0; i < str.length(); i++)
+	{
+		if (std::isalpha(str[i]) || std::isdigit(str[i]))
+			return (0);
+	}
+	return (1);
+}
+
+std::string	askForInput(std::string prompt)
+{
+	std::string	userInput;
+
+	while (42)
+	{
+		std::cout << prompt << std::endl;
+		std::getline(std::cin, userInput);
+		if (invalidInput(userInput))
+		{
+			std::cout << "Field cannot be empty" << std::endl;
+			continue ;
+		}
+		break ;
+	}
+	return (userInput);
+}
 
 void	Contact::setContactDetails()
 {
-	std::cout << "Just checking\n";
+	Contact::firstName = askForInput("Please enter First name of Contact");
+	std::cout << "User has given a firstName of: " << Contact::firstName << std::endl;
+	/*
+	 * Get input from user.
+	 * Check if non-empty input.
+	 * Set variable.
+	 * Repeat five times.
+	*/
 }
