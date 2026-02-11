6af507795b412646a87af030a6d74762820208ff
TerraBreus <studentbreus@gmail.com>
2026-02-11 11:41:36 +0100

    feat: printing Brief Contact Information on search cmd

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,38 +6,25 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 10:21:24 by zivanov           #+#    #+#             */
-/*   Updated: 2026/02/09 14:38:23 by zivanov          ###   ########.fr       */
+/*   Updated: 2026/02/11 11:35:04 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
-#include <iostream>
-#include <iomanip>
-#include <cstdlib>
-#include <string>
 
 PhoneBook::PhoneBook() : index(-1), numContacts(0)
 {
 	std::cout << "W E L C O M E  T O  T H E  8 0 ' S\n";
 }
 
-/*
- *	Once all the fields have been completed,
- *	add the contact to the phonebook.
- *
- *	Meaning we must first create a class for the newContact,
- *	fill it with the relavant information, and then add it to
- *	the array. Currently we fill in the empty ContactEntry that
- *	is already stored in the PHoneBOok. Small difference but the
- *	subject does explicitly state we must first set the details
- *	of the contact and THEN add it to the phonebook.
-*/
-
 void	PhoneBook::add()
 {
+	Contact	newContact;
+
 	index = (index + 1) % MAX_CONTACTS;
 	std::cout << "Adding Contact with index number:" << index << "\n";
-	contacts[index].setContactDetails();
+	newContact.setContactDetails();
+	contacts[index] = newContact;
 	if (numContacts < MAX_CONTACTS)
 		numContacts++;
 
@@ -59,7 +46,13 @@ void	PhoneBook::printBriefContactDetails()
 {
 	for (int i = 0; i < numContacts; i++)
 	{
+		std::cout << std::setw(TABLE_WIDTH) << i;
+		std::cout << "|";
 		printElementInWidth(contacts[i].getFirstName(), TABLE_WIDTH);
+		std::cout << "|";
+		printElementInWidth(contacts[i].getLastName(), TABLE_WIDTH);
+		std::cout << "|";
+		printElementInWidth(contacts[i].getNickName(), TABLE_WIDTH);
 		std::cout << std::endl;
 	}
 }
@@ -67,6 +60,7 @@ void	PhoneBook::printBriefContactDetails()
 void	PhoneBook::search()
 {
 	printBriefContactDetails();
+	askForInput("Enter index to view full contact information");
 	/*
 	 * Print Initial Contact information
 	 * index - firstName - lastName - nickName
